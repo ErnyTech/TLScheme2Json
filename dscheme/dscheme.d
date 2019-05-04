257010deee84ce73c77fdc4366d1f1c9c95a598e
Ernesto Castellotti <erny.castell@gmail.com>
2019-05-04 22:22:10 +0200

    Generates the schema instead of downloading the pre-generated

--- dscheme/dscheme.d
+++ dscheme/dscheme.d
@@ -1,18 +1,16 @@
 module dscheme;
 
 import tlscheme2json;
-enum DEFAULT_SCHEME_URL = "https://raw.githubusercontent.com/ErnyTech/TLScheme2Json/master/TLScheme.json";
 
 class DScheme {
     private static TLClass[] tlClasses;
 
     static this() {
-        import std.stdio : writeln;
-        import std.net.curl : get;
         import asdf : deserialize;
  
-        writeln("Obtaining TLScheme Json from " , DEFAULT_SCHEME_URL);
-        auto jsonScheme = get(DEFAULT_SCHEME_URL).dup;
+        auto parser = new TLScheme2Json();
+        parser.parse;
+        auto jsonScheme = parser.toJson;
         auto tljson = deserialize!TLJson(jsonScheme);
         this.tlClasses = tljson.tl_classes;
     }
